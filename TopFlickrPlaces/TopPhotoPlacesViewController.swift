c849bc42ec93c5a9bc25e348036d31287d37c0d0
anatoliykant <kant.anatoliy@yandex.ru>
2016-04-25 00:29:46 +0300

    why ---

--- TopFlickrPlaces/TopPhotoPlacesViewController.swift
+++ TopFlickrPlaces/TopPhotoPlacesViewController.swift
@@ -13,69 +13,23 @@ import UIKit
 class CatsViewController: UIViewController {
     
     
-    var apiClient = APIClient()
-    var photosTop:[PhotoTopPlaces]?
+//    var apiClient = APIClient()
+//    var photosTop:[PhotoTopPlaces]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //setup()
-    }
+    }   
     
-//    func setup() {
-//        mapView.delegate = self
+//    @IBAction func showMeTop100Places() {
 //        
-//        locationManager.delegate  = self
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-//        locationManager.startUpdatingLocation()
+//        //Список 100 лучших мест Flickr
+//        apiClient.findTopPlaces("22")  { (success, failure) -> Void in
+//            
+//            self.photosTop = success
+//            
+//        }        
 //    }
-    
-    
-//    @IBAction func showMeTheCats(sender: AnyObject) {
-//        
-//        let radius:Double = 5
-//        apiClient.find("cat",
-//                       longitude: mapView.centerCoordinate.longitude,
-//                       latitude: mapView.centerCoordinate.latitude,
-//                       radius: radius) { (success, failure) -> Void in
-//                        
-//                        self.photos = success
-//                        self.updateMapView()
-//                        
-//        }
-//    }
-    
-//    func updateMapView(){
-//        
-//        self.mapView.removeAnnotations(self.mapView.annotations)
-//        
-//        if self.photos != nil {
-//            self.mapView.addAnnotations(self.photos!)
-//        }
-//        
-//    }
-//    
-//    func updateMapViewTop(){
-//        
-//        self.mapView.removeAnnotations(self.mapView.annotations)
-//        
-//        if self.photosTop != nil {
-//            self.mapView.addAnnotations(self.photosTop!)
-//        }
-//        
-//    }
-    
-    
-    @IBAction func showMeTop100Places() {
-        
-        //Список 100 лучших мест Flickr
-        apiClient.findTopPlaces("22")  { (success, failure) -> Void in
-            
-            self.photosTop = success
-            
-        }
-        
-    }
 }
 
 
